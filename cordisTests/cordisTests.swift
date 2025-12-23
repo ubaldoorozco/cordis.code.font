9d6c6ae0881d6f92ccbf46f7f5de444f83a7a364
balo.orozco1@icloud.com <balo.orozco1@icloud.com>
2025-12-23 13:49:25 -0600

    Initial Commit

---
+++ cordisTests/cordisTests.swift
@@ -0,0 +1,17 @@
+//
+//  cordisTests.swift
+//  cordisTests
+//
+//  Created by Balo Orozco on 23/12/25.
+//
+
+import Testing
+@testable import cordis
+
+struct cordisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
